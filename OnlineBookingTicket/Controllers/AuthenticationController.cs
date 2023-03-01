42228dc85a7870585bce11ed00ebcec0e1b2bd7c
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-03-01 20:29:11 +0700

    Added Layout login

---
+++ OnlineBookingTicket/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineBookingTicket.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
