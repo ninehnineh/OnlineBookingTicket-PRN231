0d2382ab912ed11dffb5bc6913c478aab6111c3e
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2023-03-11 18:31:33 +0700

    Login, Logout APIs

---
+++ OnlineBookingTicketAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using DTO.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.AuthenticationRepository;
+
+namespace OnlineBookingTicketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationRepository _authenticationRepository;
+
+        public AuthenticationController(IAuthenticationRepository authenticationRepository)
+        {
+            _authenticationRepository = authenticationRepository;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(AuthenRequest request)
+        {
+            var user = await _authenticationRepository.Login(request);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        {
+            return Ok(await _authenticationRepository.Register(request));
+        }
+    }
+}
